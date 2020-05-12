9a9096316870f67b8659452519975493801e0d33
Prem Kumar <prmsrswt@gmail.com>
2020-05-12 19:05:50 +0530

    optimize for mobile

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,51 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./nav.module.css";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className={styles.nav}>
-      <Link className={styles.link} to="/">
-        Home
-      </Link>
-      <Link className={styles.link} to="/papers">
-        Call for Papers
-      </Link>
-      <Link className={styles.link} to="/schedule">
-        Important Dates/Schedules
-      </Link>
-      <Link className={styles.link} to="/paper-submission">
-        Paper Submission
-      </Link>
-      <Link className={styles.link} to="/registration">
-        Registration
-      </Link>
-      <Link className={styles.link} to="/accepted-papers">
-        Accepted Papers
-      </Link>
-      <Link className={styles.link} to="/committees">
-        Committees
-      </Link>
-      <Link className={styles.link} to="/keynote">
-        Keynote Speakers
-      </Link>
-      <Link className={styles.link} to="/sponsors">
-        Sponsors
-      </Link>
-      <Link className={styles.link} to="/accommodation">
-        Accommodation
-      </Link>
-      <Link className={styles.link} to="/past-conferences">
-        Past Conferences
-      </Link>
-      <Link className={styles.link} to="/activities">
-        City Tours & Activities
-      </Link>
-      <Link className={styles.link} to="/contact">
-        Contact Us
-      </Link>
-    </nav>
+    <>
+      <div className={styles.hamdiv}>
+        <button
+          type="button"
+          className={`${styles.btn} ${isOpen && styles.open}`}
+          onClick={() => setIsOpen((o) => !o)}
+        />
+      </div>
+      <nav
+        className={`${styles.nav} ${isOpen && styles.navopen}`}
+        onClick={() => setIsOpen((o) => !o)}
+      >
+        <Link className={styles.link} to="/">
+          Home
+        </Link>
+        <Link className={styles.link} to="/papers">
+          Call for Papers
+        </Link>
+        <Link className={styles.link} to="/schedule">
+          Important Dates/Schedules
+        </Link>
+        <Link className={styles.link} to="/paper-submission">
+          Paper Submission
+        </Link>
+        <Link className={styles.link} to="/registration">
+          Registration
+        </Link>
+        <Link className={styles.link} to="/accepted-papers">
+          Accepted Papers
+        </Link>
+        <Link className={styles.link} to="/committees">
+          Committees
+        </Link>
+        <Link className={styles.link} to="/keynote">
+          Keynote Speakers
+        </Link>
+        <Link className={styles.link} to="/sponsors">
+          Sponsors
+        </Link>
+        <Link className={styles.link} to="/accommodation">
+          Accommodation
+        </Link>
+        <Link className={styles.link} to="/past-conferences">
+          Past Conferences
+        </Link>
+        <Link className={styles.link} to="/activities">
+          City Tours & Activities
+        </Link>
+        <Link className={styles.link} to="/contact">
+          Contact Us
+        </Link>
+      </nav>
+    </>
   );
 };
 
